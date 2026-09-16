ace5993b225f225f4f366758e3e9d4ece09cb572
carollvieira <mcarolsvncv@usp.br>
2026-09-16 13:44:30 -0300

    integra cbuild principal, corrige config

--- lib/config.sh
+++ lib/config.sh
@@ -1,38 +1,14 @@
 #!/bin/bash
 
-source ./lib/errors.sh
-source ./lib/logs.sh
-source ./include/extra.sh
-source ./lib/config.sh
-source ./lib/build.sh
-source ./lib/rebuild.sh
-source ./lib/clean.sh
-source ./lib/info.sh
-source ./lib/run.sh
+#load_config() carrega as configurações do cbuild.conf, caso ele exista. Caso não exista, carrega as configurações padrão.
+#argumentos: nenhum
+#retorno: nenhum
+load_config() {
+    EXEC_NAME="a.out"
+    CFLAGS="-Wall"
+    BUILD_DIR="build"
 
-#Carrega as configurações
-load_config
-
-#Obtém o comando passado como primeiro argumento 
-comando="$1"
-#Executa a função correspondente ao comando digitado
-case "$comando" in
-	build)
-		build
-		;;
-	rebuild)
-		rebuild
-		;;
-	clean)
-		clean
-		;;
-	info)
-		info
-		;;
-	run)
-		run
-		;;
-	*)
-		error_command
-		;;
-esac
+    if [ -f "cbuild.conf" ]; then
+        source "cbuild.conf"
+    fi
+}
